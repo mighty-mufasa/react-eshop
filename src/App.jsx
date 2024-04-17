8d442d7c843731b8f0b2943784052d002727f8ca
Keigo Igarashi <keigo263129@outlook.com>
2024-04-17 14:39:57 -0300

    Add Footer component and update App.jsx to include Footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { Outlet } from 'react-router-dom'
 import './App.css'
-import Header from './components/Header'
+import Header from './components/Header/Header'
+import Footer from './components/Footer/Footer'
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <main>
         <Outlet />
       </main>
+      <Footer />
     </>
   )
 }
---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,17 @@
+import './Footer.css'
+
+export default function Footer() {
+  return (
+    <footer>
+      <ul>
+        <li>Site Map</li>
+        <li>Contact</li>
+        <li>About us</li>
+      </ul>
+      <p>
+        Created by{' '}
+        <a href="https://github.com/andres-verdenelli">Andrés Verdenelli</a>
+      </p>
+    </footer>
+  )
+}
