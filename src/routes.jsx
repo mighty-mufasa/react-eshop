b22612110bb4c9c1cc9ede20426c492fc96e2ff0
Keigo Igarashi <keigo263129@outlook.com>
2024-04-17 11:59:02 -0300

    Add routes.jsx for routing configuration

---
+++ src/routes.jsx
@@ -0,0 +1,20 @@
+import Home from './components/Home'
+import App from './App'
+
+export const routes = [
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <h1>404 error, page don&apos;t found</h1>,
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      },
+      {
+        path: '/store',
+        element: <h1>Welcome to store</h1>,
+      },
+    ],
+  },
+]
