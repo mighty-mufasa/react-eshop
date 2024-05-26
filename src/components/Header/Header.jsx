77cdc3a17f5bf2cfd700ec318cfeeee66354c8b2
Keigo Igarashi <keigo263129@outlook.com>
2024-05-26 11:38:49 -0300

    style: Update Header component with new navigation links

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -8,7 +8,12 @@ export default function Header() {
           <Link to="/">
             <h1>Gadget Galaxy 🪐</h1>
           </Link>
-          <button>
+          <div className="navbar">
+            <Link to="/">Home</Link>
+            <Link to="/store">Store</Link>
+            <Link to="login">Login</Link>
+          </div>
+          <button className="burgerButton">
             <span className="material-symbols-outlined">menu</span>
           </button>
         </div>
